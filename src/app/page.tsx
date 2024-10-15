72dce5838f2b66bf28b8306b82981ea24619af1d
あさり <136340019+isshi0306@users.noreply.github.com>
2024-10-15 09:01:49 +0900

    Update global CSS variables and add metadata to layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "トップページ | スポンサーサイト | 高専カンファレンス in 首都",
+  description: "高専カンファレンス in 首都のスポンサーサイトです。",
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_DOMAIN}`,
+  },
+};
 
 export default function Home() {
   return (
